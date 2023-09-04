f192db80b7764beab3da311c93bb6d3f9f634d65
Yulia Yamalieva <yulia.yamalieva@yandex.ru>
2023-09-04 18:54:45 +0300

    Исправила валидацию для фронта

--- utils/constants.js
+++ utils/constants.js
@@ -1,9 +1,8 @@
 const OK_STATUS = 200;
 const CREATED_STATUS = 201;
 // Регулярное выражение для проверки URL
-const regexLink = /^https?:\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\\/~+#-]*[\w@?^=%&\\/~+#-])/im;
-// Регулярное выражение для проверки URL картинки
-const regexImage = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
+const regexLink = /^https?:\/\/[^\s/$.?#].[^\s]*$/;
+
 
 module.exports = {
   OK_STATUS,
